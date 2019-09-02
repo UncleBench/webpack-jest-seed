0d1f00eb4b2f626a336f1ea8100314a951f9c63e
LAG Valentin Buergler <Valentin.Buergler@leuchterag.ch>
2019-09-02 16:25:01 +0200

    Solved excercise

--- src/scripts/classes/classes.js
+++ src/scripts/classes/classes.js
@@ -1,3 +1,4 @@
+// using class
 class Person {
     constructor(name) {
         this.name = name;
@@ -7,5 +8,14 @@ class Person {
         console.log(`Hello ${this.name}`);
     }
 }
+
+// // using functions
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.sayHello = function() {
+//     console.log(`Hello ${this.name}`);
+// }
 
 module.exports = Person;
